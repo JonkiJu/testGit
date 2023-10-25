1ecfc8ac8d3ba31f535705a3a5634990fa5b4455
Vadym <vadima.opudaa@gmail.com>
2023-10-25 16:47:24 +0300

    commit

---
+++ JS/script.js
@@ -0,0 +1,208 @@
+'use strict';
+
+// a = 15;
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// leftBorderWidth = 10;
+// console.log(leftBorderWidth);
+
+// // const obj = {
+// // 	a: 50
+// // };
+
+// // obj.a = 10;
+
+// console.log (obj);
+
+// var name = "Ivan";
+
+
+// {
+// 	let result = 50;
+// }
+
+// console.log(result);
+
+
+
+// const numbe = 1;
+ 
+// const word = "petro";
+
+// const bool = true;
+
+// let und;
+// console.log(und);
+
+///////////
+
+
+// const obj = {
+// 	name: "John",
+// 	age: 25,
+// 	isMarried: false
+// };
+// console.log(obj["name"]);
+
+// let arr = ["plun.png", "orange.jpg", "huyhna.sys",{}, []];
+// console.log(arr[1]);
+
+
+
+
+// УРОК 12
+
+// const arr = ["a", "b", "c"];
+// // const obj = {a: 1, b: 2};
+
+
+
+// const arrObj = {
+// 	a: "a",
+// 	1: "ba",
+// 	2: "c"
+// };
+
+
+
+// arrObj.1 = 3;
+// arrObj.b = "123";
+
+// console.log(arrObj);
+
+
+// const b = [1,2,3,4];
+	
+
+
+
+// console.log(b);
+
+
+
+
+// obj = {
+// 	"Anna": 500,
+// 	"Alice": 800,
+	
+// };
+
+// console.log(obj["Anna"]);
+
+
+
+
+
+
+// let storeName = 'Mono';
+
+// const storeDescription = {
+// 	budget: 10000,
+//     employees: ["Peter", "Luis", "Vadim"],
+//     products: {
+//         weed: 400,
+//         lsd: 500
+//     },
+//     open: true
+// };
+
+
+// console.log(storeDescription['products'])
+
+
+// const storeName = 'My awesome store';
+
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Alex', 'John', 'Sam'],
+//     products: {
+//         'soap': 20,
+//         'water': 5
+//     },
+//     open: true
+// }
+
+
+// УРОК 13
+
+// alert('Hello'); 
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам є 18?', '');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Як ваше імя?', '');
+// answers[1] = prompt('Як ваше Фамілія?', '');
+// answers[2] = prompt('Скільки вам років?', '');
+
+// // document.write(answers);
+// console.log(typeof(answers));
+
+
+// УРОК 14 
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5/10/${category}`);
+
+// const user = 'Ivan';
+// alert(`привет, ${user}`);
+
+
+
+// УРОК 15      ОПЕРАТОРИ
+
+console.log('arr' + '');
+
+let incr = 10,
+	decr = 10;   
+
+incr++;         // їх можна писати префіксною формою, тобто спереду ++incr, або постфіксною формою, тобто після самого значення decr--,
+decr--;         // але якщо в консоль лог написати постфіксну форму то саме значення виведеться без змін, тому що ++ або -- прорахувався після виведення значення
+
+console.log(incr);
+console.log(decr);
+
+console.log(10%2);   // тут перше число ділиться на друге число стільки скільки може, а те що лишилось виводиться в консоль
+
+console.log(2*4 == 8);      // це просто зрівнення == воно покаже чи співпадають два значенння, і тип обєкту не грає різниці
+console.log(2*4 === 8);      // це строге зрівнянн === воно покаже чи співпадають два значенння, але тип обєкту також має співпадати
+
+
+const isChecked = true,
+	  isClose = true;
+	  isDuo = true;
+
+
+console.log(isChecked && !isClose);       // це логічний оператор "І" && він показує чи ці два обєкти рівні в вигляді Boolean або ж простими словами true/false
+console.log(isChecked || isClose);       // це логічний оператор "АБО" || він показує чи хочаби один з обєктів правдивий
+console.log(isChecked || isClose);       // це логічний оператор "ОТРІЦАНІЯ" ! він міняє значення обєкта
+
+
+console.log(2 + 2 * 2 == 8);      // це просто зрівнення == воно покаже чи співпадають два значенння, і тип обєкту не грає різниці
+console.log(2*4 != 8);      // це просто зрівнення НЕ РАВНО != якщо два обєкта не співпадають, то воно покаже TRUE 
+
+
+// УРОК 16
+
+// GIT 
+
+
+
+
+
+
+
+
+
+
